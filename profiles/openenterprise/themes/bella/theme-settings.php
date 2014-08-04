600b6d8eceff4f24e717e1e17ed969b0c0df7576
Kyle Taylor <kyletaylored@gmail.com>
2014-08-03 19:14:51 -0500

    Added Gist link to description

--- profiles/openenterprise/themes/bella/theme-settings.php
+++ profiles/openenterprise/themes/bella/theme-settings.php
@@ -19,7 +19,7 @@ function bella_form_system_theme_settings_alter(&$form, &$form_state) {
 		'#type' => 'textarea',
 		'#title' => t('Kuler JSON'),
 		'#default_value' => theme_get_setting('bella_kuler_json'),
-		'#description' => t('If you have Kuler JSON, you can enter it here to provide theme color options.'),
+		'#description' => t('If you have Kuler JSON, you can enter it here to provide theme color options. You can get Kuler JSON by using this gist: !gist', array('!gist' => l('Kuler JSON Gist', 'https://gist.github.com/kyletaylored/1a2edba96509277cfcca', array('attributes' => array('target' => '_blank',))))),
 	);
 	
 }
