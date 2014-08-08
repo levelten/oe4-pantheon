7db280b043fcc58267e4fddc579d42d60967d157
Kyle Taylor <kyletaylored@gmail.com>
2014-08-08 12:22:59 -0500

    Updated Kuler/Colourlovers theme form

--- profiles/openenterprise/themes/bella/theme-settings.php
+++ profiles/openenterprise/themes/bella/theme-settings.php
@@ -1,35 +1,47 @@
 <?php
 
 function bella_form_system_theme_settings_alter(&$form, &$form_state) {
-	$form['general']['#weight'] = -8;
 
-	$form['bella'] = array(
-		'#type' => 'vertical_tabs',
-		'#prefix' => '<h2><small>Bella</small></h2>',
+	$form['kuler_settings'] = array(
+		'#type' => 'fieldset',
+		'#title' => t('Adobe Kuler Settings'),
+		'#description' => t('Adding in text for JSON Kuler'),
 		'#weight' => -1,
 	);
-
-	$form['bella_config'] = array(
-		'#type' => 'fieldset',
-		'#group' => 'bella',
-		'#title' => t('Kuler JSON'),
-		'#description' => t('Adding in text for JSON Kuler'),
-	);
-	$form['bella_config']['bella_kuler'] = array(
+	$form['kuler_settings']['kuler_check'] = array(
 		'#type' => 'checkbox',
-		'#title' => t('Use Kuler?'),
-		'#default_value' => theme_get_setting('bella_kuler'),
+		'#title' => t('Use Adobe Kuler?'),
+		'#default_value' => theme_get_setting('kuler_check'),
 		'#options' => array(
 			0 => t('No'),
 			1 => t('Yes'),
 		),
 		'#description' => t('Checking this will replace the current color schemes with those provided by the Kuler JSON.'),
 	);
-	$form['bella_config']['bella_kuler_json'] = array(
+	$form['kuler_settings']['kuler_json'] = array(
 		'#type' => 'textarea',
 		'#title' => t('Kuler JSON'),
-		'#default_value' => theme_get_setting('bella_kuler_json'),
+		'#default_value' => theme_get_setting('kuler_json'),
 		'#description' => t('If you have Kuler JSON, you can enter it here to provide theme color options. You can get Kuler JSON by using this gist: !gist', array('!gist' => l('Kuler JSON Gist', 'https://gist.github.com/kyletaylored/1a2edba96509277cfcca', array('attributes' => array('target' => '_blank',))))),
 	);
-	
+
+	$form['colourlovers_settings'] = array(
+		'#type' => 'fieldset',
+		'#title' => t('COLOURLovers Settings'),
+		'#weight' => -1,
+	);
+	$form['colourlovers_settings']['colourlovers_check'] = array(
+		'#type' => 'checkbox',
+		'#title' => t('Enable COLOURLovers palettes'),
+		'#default_value' => theme_get_setting('colourlovers_check'),
+		'#options' => array(
+			0 => t('No'),
+			1 => t('Yes'),
+		),
+		'#description' => t('Checking this will replace the current color schemes with those provided by the !colourlover.', array('!colourlover' => l('COLOURLovers API', 'admin/appearance/colourlovers'))),
+	);
+	$form['colourlovers_settings']['colourlovers_demo'] = array(
+  	'#markup' => variable_get('colourlovers_demo_html', 'No palettes generated.'),
+  );
+  
 }
