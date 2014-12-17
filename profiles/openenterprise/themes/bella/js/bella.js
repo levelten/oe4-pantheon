bc7232fddaa8c9b7e4958b222ec2f9c26351d4c9
Kyle Taylor <kyletaylored@gmail.com>
2014-12-17 14:35:22 -0600

    Fix header/logo/name stuff

--- profiles/openenterprise/themes/bella/js/bella.js
+++ profiles/openenterprise/themes/bella/js/bella.js
@@ -27,22 +27,22 @@
         el.style.width = navbarChildWidth;
       });
       
-      // Set height of header for logo positioning.
-      var brand = navbar.querySelector(".navbar-header .navbar-brand");
-      var brandHeight = brand.offsetHeight;
+      // // Set height of header for logo positioning.
+      // var brand = navbar.querySelector(".navbar-header .navbar-brand");
+      // var brandHeight = brand.offsetHeight;
       var navbarHeight = navbar.offsetHeight;
 
-      // If the branding area is smaller than the navigation, vertically align the branding (name, logo).
-      if (brandHeight < navbarHeight) {
-        var regionHeight = navbar.querySelector(".region-navigation").offsetHeight;
-        brand.style.maxHeight = (regionHeight-10)+"px";
-        navbar.querySelector(".navbar-header").style.height = context.getElementById("navbar").offsetHeight+"px";
-        if (brand.classList) {
-          brand.classList.add("vertical-align");
-        } else {
-          brand.className += " " + "vertical-align";
-        }
-      }
+      // // If the branding area is smaller than the navigation, vertically align the branding (name, logo).
+      // if (brandHeight < navbarHeight) {
+      //   var regionHeight = navbar.querySelector(".region-navigation").offsetHeight;
+      //   brand.style.maxHeight = (regionHeight-10)+"px";
+      //   navbar.querySelector(".navbar-header").style.height = context.getElementById("navbar").offsetHeight+"px";
+      //   if (brand.classList) {
+      //     brand.classList.add("vertical-align");
+      //   } else {
+      //     brand.className += " " + "vertical-align";
+      //   }
+      // }
 
       // Add extra pixels if we're logged in.
       if ($("body", context).hasClass("admin-menu")){
@@ -54,6 +54,11 @@
       // Add padding to body.
       var bodyHtml = context.querySelector("body.html");
       bodyHtml.style.paddingTop = navbarHeight+"px";
+      // Update based on window resizing.
+      $(window).resize(function(){
+        navbarHeight = context.getElementById("navbar").offsetHeight;
+        bodyHtml.style.paddingTop = navbarHeight+"px";
+      });
 
       // Only do the following if the navbar is fixed.
       // var navbarHasClass = $navbar.hasClass("navbar-fixed-top");
@@ -68,13 +73,7 @@
           navbarHeight = context.getElementById("navbar").offsetHeight;
           dropdownMenu.style.top = navbarHeight+"px";
         });
-      }
-
-      // Update based on window resizing.
-      $(window).resize(function(){
-        navbarHeight = context.getElementById("navbar").offsetHeight;
-        bodyHtml.style.paddingTop = navbarHeight+"px";
-      });*/
+      }*/
 
     }
   };
