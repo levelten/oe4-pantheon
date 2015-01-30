7529821460989f8ac3a36c39eae8aaf8f7fd3f67
Kyle Taylor <kyletaylored@gmail.com>
2015-01-29 20:45:38 -0600

    Enterprise Extras: move responsive tabs to theme as setting
    Also cleaned up JS options a bit.

--- profiles/openenterprise/modules/oe/enterprise_extras/js/enterprise_extras.js
+++ profiles/openenterprise/modules/oe/enterprise_extras/js/enterprise_extras.js
@@ -83,21 +83,4 @@
         }
     };
 
-    /**
-     * Responsive Tabs
-     */
-    if ($.fn.responsiveTabs) {
-        Drupal.behaviors.responsiveTabs = {
-            attach: function(context) {
-                // Make tabs responsive.
-                $('ul.nav-tabs, ul.tabs.primary, ul.tabs.secondary, ul.quicktabs-tabs, ul.horizontal-tabs-list', context).responsiveTabs({
-                    activeClass: 'active',
-                    activeSelectors: '.active, .selected, .active-trail',
-                    icon: '<i aria-hidden="true" class="icon fontello oeicon-down-open"></i>',
-                    text: 'More'
-                });
-            }
-        };
-    }
-
 })(jQuery);
