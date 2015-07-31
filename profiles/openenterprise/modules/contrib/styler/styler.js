073979a40083dd200491899ba1ee933329d5acba
Ahmad Kharbat <ahmadk@getlevelten.com>
2015-07-31 15:22:07 -0500

    Synchronize style guide sidebar with the page content

--- profiles/openenterprise/modules/contrib/styler/styler.js
+++ profiles/openenterprise/modules/contrib/styler/styler.js
@@ -5,6 +5,9 @@
     Drupal.behaviors.stylerView = {
         attach: function(context, settings) {
             window.prettyPrint && prettyPrint();
+
+            // Activates scrollspy on sidebar scrollbar
+            $('body').scrollspy({ target: '#style-guide-index' });
         }
     }
 
