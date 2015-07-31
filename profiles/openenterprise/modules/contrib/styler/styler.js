995460a04a3a26dd699ac5b390f7fd460eea7464
Ahmad Kharbat <ahmadk@getlevelten.com>
2015-07-31 16:09:13 -0500

    Improve style guide scroll bar behavior to adjust position while scrolling

--- profiles/openenterprise/modules/contrib/styler/styler.js
+++ profiles/openenterprise/modules/contrib/styler/styler.js
@@ -7,7 +7,15 @@
             window.prettyPrint && prettyPrint();
 
             // Activates scrollspy on sidebar scrollbar
-            $('body').scrollspy({ target: '#style-guide-index' });
+            $('body').scrollspy({ target: '#style-guide-index', offset:100 });
+
+            // Activates affix on sidebar scrollbar to adjust position while scrolling
+            $('#style-guide-index .nav').affix({
+                offset: {
+                    top: 400,
+                    bottom: $('footer').outerHeight(true) + 100
+                }
+            });
         }
     }
 
