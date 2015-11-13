9f67ac41f6100e40fa3573db2cb891822e9a00bb
Kyle Taylor <kyletaylored@gmail.com>
2015-11-13 08:05:59 -0600

    Health: Add nav-pills/nav-stacked to menu block wrapper

--- profiles/openenterprise/themes/enterprise_health/template.php
+++ profiles/openenterprise/themes/enterprise_health/template.php
@@ -5,3 +5,9 @@
  * template.php
  */
 
+/**
+ * Overrides theme_menu_tree().
+ */
+function enterprise_health_menu_tree__menu_block(&$variables) {
+  return '<ul class="menu nav nav-stacked nav-pills">' . $variables['tree'] . '</ul>';
+}
