f84776c2ffd36b112d064d094431e588ef06b4b5
Tom McCracken <tomm@getlevelten.com>
2015-12-02 17:28:35 -0600

    adding facet filter display options

--- profiles/openenterprise/modules/oe/enterprise_rel/js/enterprise_rel.node_type_form.js
+++ profiles/openenterprise/modules/oe/enterprise_rel/js/enterprise_rel.node_type_form.js
@@ -6,8 +6,8 @@
     attach: function (context) {
       // Provide the vertical tab summaries.
       $('fieldset#edit-enterprise-rel', context).drupalSetSummary(function (context) {
-        var vdVal = $("#edit-view-display option:selected").val();
-        var vdText = $("#edit-view-display option:selected").text();
+        var vdVal = $("#edit-enterprise-rel-view-display option:selected").val();
+        var vdText = $("#edit-enterprise-rel-view-display option:selected").text();
         if (!vdVal) {
           return Drupal.t('None');
         }
