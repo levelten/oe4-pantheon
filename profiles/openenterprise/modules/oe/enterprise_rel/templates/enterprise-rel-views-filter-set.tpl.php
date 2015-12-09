abcd6f3dc5683e3fd29b06493e55a10b53436498
Tom McCracken <tomm@getlevelten.com>
2015-12-09 17:39:08 -0600

    cleaning up views facet filter, adding premium offer support to enterprise conversion

--- profiles/openenterprise/modules/oe/enterprise_rel/templates/enterprise-rel-views-filter-set.tpl.php
+++ profiles/openenterprise/modules/oe/enterprise_rel/templates/enterprise-rel-views-filter-set.tpl.php
@@ -12,7 +12,7 @@
   <?php foreach ($rows as $id => $row): ?>
     <li class="<?php print (!empty($list_item_classes) ? ' ' . $list_item_classes : '') . (!empty($row_list_item_classes[$id]) ? ' ' . $row_list_item_classes[$id] : ''); ?>">
       <a href="<?php print $row->url; ?>"<?php print !empty($row_classes[$id]) ? ' class="'. $row_classes[$id] .'"' : ''; ?>>
-        <?php if (!empty($options['count'])): ?>
+        <?php if (!empty($options['count']) && !empty($row->count)): ?>
           <span class="badge<?php print !empty($row_badge_classes[$id]) ? ' ' . $row_badge_classes[$id] : ''; ?>"><?php print $row->count?></span>
         <?php endif; ?>
         <?php print $row->link; ?>
