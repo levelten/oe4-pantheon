0029005738f0c031ae5a8608a03cf1cebe9743dc
Kyle Taylor <kyletaylored@gmail.com>
2015-12-17 16:30:29 -0600

    Fix theming function, add conditions to JS

--- profiles/openenterprise/modules/oe_ext/blueimp/blueimp_fields/blueimp_fields.js
+++ profiles/openenterprise/modules/oe_ext/blueimp/blueimp_fields/blueimp_fields.js
@@ -10,45 +10,54 @@ var Drupal = Drupal || {};
 (function($, Drupal) {
     "use strict";
 
+    function blueimp_lightbox(link_id) {
+        document.getElementById(link_id).onclick = function(event) {
+            event = event || window.event;
+            var target = event.target || event.srcElement,
+                link = target.src ? target.parentNode : target,
+                options = {
+                    index: link,
+                    event: event
+                },
+                links = this.getElementsByTagName('a');
+            blueimp.Gallery(links, options);
+        };
+    }
+
+    function blueimp_carousel(link_id, gallery_id) {
+        blueimp.Gallery(
+            document.getElementById(link_id).getElementsByTagName('a'), {
+                container: "#"+gallery_id,
+                carousel: true,
+                thumbnailIndicators: true,
+            }
+        );
+    }
+
     Drupal.behaviors.blueimp_fields = {
         attach: function(context, settings) {
 
-            window.console.log(settings);
-            window.console.log(typeof blueimp);
+                if (settings.blueimp_fields && settings.blueimp_fields.slides) {
 
-            if (settings.blueimp_fields && settings.blueimp_fields.slides) {
+                    var slides = settings.blueimp_fields.slides;
 
-                var slides = settings.blueimp_fields.slides;
+                    for (var i = slides.length - 1; i >= 0; i--) {
+                        window.console.log(slides[i]);
 
-                for (var i = slides.length - 1; i >= 0; i--) {
-                    window.console.log(slides[i]);
-                }
+                        if (slides[i].display_style === "lightbox") {
+                            blueimp_lightbox(slides[i].link_id);
 
-                document.getElementById('blueimp-links-1').onclick = function(event) {
-                    event = event || window.event;
-                    var target = event.target || event.srcElement,
-                        link = target.src ? target.parentNode : target,
-                        options = {
-                            index: link,
-                            event: event
-                        },
-                        links = this.getElementsByTagName('a');
-                    blueimp.Gallery(links, options);
-                };
-
-                blueimp.Gallery(
-                    document.getElementById('blueimp-links-1').getElementsByTagName('a'), {
-                        container: '#blueimp-gallery-1',
-                        carousel: true
+                        } else if (slides[i].display_style === "carousel") {
+                            blueimp_carousel(slides[i].link_id, slides[i].gallery_id);
+                        }
                     }
-                );
 
-            } else {
-                window.console.log("Something is wrong with the Blueimp gallery settings.");
-            }
-            // var gallery = $('#blueimp-gallery-1').data('gallery');
+                } else {
+                    window.console.log("Something is wrong with the Blueimp gallery settings.");
+                }
+                // var gallery = $('#blueimp-gallery-1').data('gallery');
 
-        }
-    };
+            } // end attach
+    }; // Drupal.behaviors
 
 })(jQuery, Drupal);
