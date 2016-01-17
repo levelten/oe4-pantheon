912bf2ba93520e32d9f5c30703651c31ee84838b
Tom McCracken <tomm@getlevelten.com>
2016-01-17 14:46:30 -0600

    cleaning up theme settings

--- profiles/openenterprise/themes/enterprise_bootstrap/template.php
+++ profiles/openenterprise/themes/enterprise_bootstrap/template.php
@@ -103,6 +103,7 @@ function enterprise_bootstrap_preprocess_page(&$variables) {
    * Enterprise Header settings
    */
   $variables['nav_left_display'] = theme_get_setting('nav_left_display');
+  dsm($variables['nav_left_display']);
   if (empty($variables['nav_left_display'])) {
     $variables['nav_left_display'] = 'standard';
   }
@@ -110,6 +111,7 @@ function enterprise_bootstrap_preprocess_page(&$variables) {
     $variables['nav_left_display'] = '';
     $variables['navbar_classes_array'][] = 'navbar-no-header';
   }
+  dsm($variables['nav_left_display']);
 
   /*
    * Enterprise Megamenu settings
