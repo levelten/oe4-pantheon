7c6fcbf32a9dc23a8b67e3a949ec6586915f76c7
Tom McCracken <tomm@getlevelten.com>
2016-05-26 14:14:26 -0500

    adding support for visitor segments

--- profiles/openenterprise/modules/oe/enterprise_rel/js/enterprise_rel.views_ajax.js
+++ profiles/openenterprise/modules/oe/enterprise_rel/js/enterprise_rel.views_ajax.js
@@ -6,24 +6,40 @@
 
   Drupal.behaviors.enterpriseRel = {
     attach: function (context) {
-      // Provide the vertical tab summaries.
-      if (!done) {
-        var vars = {
-          view_dom_id: '#block-enterprise-rel-views-facet-filter',
-          //view_dom_id: '.view-id-enterprise_blog_blocks .view-display-id-related',
-          view_name: 'enterprise_blog_blocks',
-          view_display_id: 'related',
-          view_path: 'node/5?test=1',
-          view_args: '',
-          rel_add_tids: '4',
-        };
-        //loadView(vars);
-        done = 1;
+      io('addCallback', 'domReady', this.init);
+    },
+    init: function () {
+      // only execute once
+      if (done) {
+        return
+      }
+      var k, i, tid, systemPath, pt, vt, storage, tids = [];
+      var taxAttrs = Drupal.settings.enterprise_rel.intel_tax_attrs;
+      var systemPath = io('get', 'p.systemPath', '');
+      for (i = 0; i < taxAttrs.length; i++) {
+        k = taxAttrs[i];
+        vt = io('get', 'va.' + k, []);
+        // add terms on visitor
+        for (tid in vt) {
+          tids.push(tid);
+        }
       }
 
+      var vars = {
+        //view_dom_id: '#block-enterprise-rel-views-facet-filter',
+        view_dom_selector: '.view-enterprise-blog-blocks.view-display-id-rel',
+        //view_dom_id: '.view-id-enterprise_blog_blocks .view-display-id-related',
+        view_name: 'enterprise_blog_blocks',
+        view_display_id: 'rel',
+        view_path: systemPath,
+        view_args: '',
+        rel_add_tids: tids.join(','),
+      };
+      console.log(vars);
+      //loadView(vars);
+      done = 1;
     }
   };
-
 
   function loadView(data) {
     $.ajax({
@@ -33,11 +49,10 @@
       dataType: 'json',
       success: function (response) {
         console.log(response);
-        console.log(data);
         if (response[2] !== undefined) {
           var viewHtml = response[2].data;
           console.log(viewHtml);
-          $(data.view_dom_id).html(viewHtml + 'AJAX');
+          $(data.view_dom_selector).html(viewHtml + '<!-- AJAX load -->' );
           Drupal.attachBehaviors(); //check if you need this.
         }
       }
